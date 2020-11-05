50454a1b418f74d7886b17334a23ed3932e947a4
ThearaDawn <dawntheara.me@gmail.com>
2020-11-05 13:43:50 +0700

    angualr with spring security

--- spring-boot-backend-angular/src/main/java/com/ums/models/dao/InvoiceDao.java
+++
@@ -1,9 +0,0 @@
-package com.ums.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ums.models.entity.Bill;
-
-public interface InvoiceDao extends CrudRepository<Bill, Long>{
-
-}
--- spring-boot-backend-angular/src/main/java/com/ums/models/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ums.models.services;
-
-import com.ums.models.entity.User;
-
-public interface UserService {
-
-	public User findByUsername(String username);
-}
